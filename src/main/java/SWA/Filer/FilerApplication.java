f32a6f02e4a9c0c5d614ba14bad91fcc05bdb4f5
Nick <nisait01@hs-esslingen.de>
2023-05-30 21:30:22 +0200

    Hello World test

--- src/main/java/SWA/Filer/FilerApplication.java
+++ src/main/java/SWA/Filer/FilerApplication.java
@@ -2,12 +2,18 @@ package SWA.Filer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class FilerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FilerApplication.class, args);
 	}
-	//test
+	@GetMapping("/")
+	public String hello(){
+			return "Hello World";
+	}
 }
