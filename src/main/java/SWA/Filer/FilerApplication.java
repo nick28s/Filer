f12a43a946280b6bddfed3d4b7a68bd602d66bdd
Nick <nisait01@hs-esslingen.de>
2023-05-30 20:43:20 +0200

    Spring/Gradle Project Template

---
+++ src/main/java/SWA/Filer/FilerApplication.java
@@ -0,0 +1,13 @@
+package SWA.Filer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SWA/Filer/FilerApplicationTests.java
@@ -0,0 +1,13 @@
+package SWA.Filer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
