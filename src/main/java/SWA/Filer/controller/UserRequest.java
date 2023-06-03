0603b370202ffb9113946f77a8d190250a6431db
Halit <efkere.h.o@gmail.com>
2023-06-03 23:02:47 +0200

    Implemented: Create, update and delete User - REST API -UserRequest.java : Methods for UserController.java

---
+++ src/main/java/SWA/Filer/controller/UserRequest.java
@@ -0,0 +1,33 @@
+package SWA.Filer.controller;
+
+public class UserRequest {
+    private String username;
+    private String password;
+
+    public UserRequest() {
+    }
+
+    public UserRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getters and setters
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
