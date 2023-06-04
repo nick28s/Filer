dbbb1c2a0e9e65ee9d583ee712911fe2300200a8
Halit <efkere.h.o@gmail.com>
2023-06-04 23:07:08 +0200

    Group management implemented -admin rights -create,update,delete,listallgroups,adduser and delete user from a group

---
+++ src/main/java/SWA/Filer/model/GroupResponse.java
@@ -0,0 +1,30 @@
+package SWA.Filer.model;
+
+public class GroupResponse {
+    private int id;
+    private String name;
+
+    public GroupResponse() {
+    }
+
+    public GroupResponse(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
